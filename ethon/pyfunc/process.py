3f713d58b83fcac920ed94c8e3f29376232cdd3d
leomemlon <139001721+monu875@users.noreply.github.com>
2023-10-06 11:44:51 +0530

    Update and rename ethon/pyfunc.py to ethon/pyfunc/process.py

---
+++ ethon/pyfunc/process.py
@@ -0,0 +1,7 @@
+import subprocess 
+
+def bash(cmd):    
+    bashCommand = f"{cmd}"
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE) 
+    output, error = process.communicate()
+    return output, error
